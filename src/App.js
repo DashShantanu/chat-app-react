73da257ab753fc004aceb118343e91362de52c53
Shantanu Dash <papun260801@gmail.com>
2022-11-18 00:17:02 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=' text-blue-700'>
+        Hello World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDVoOPcGy0P6vU9l--UCpIOrCPVUnD1pSc",
+    authDomain: "chat-app-react-8b394.firebaseapp.com",
+    projectId: "chat-app-react-8b394",
+    storageBucket: "chat-app-react-8b394.appspot.com",
+    messagingSenderId: "918201514897",
+    appId: "1:918201514897:web:548b64688d3d356da23830",
+    measurementId: "G-5EW45W20S6"
+};
